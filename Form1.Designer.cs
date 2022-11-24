25d007c95f2e08ae0677c6754d21f0f990b0812a
tomisrb <megagamerimc@gmail.com>
2022-11-24 22:22:21 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace iksoks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.IGRA = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.novaIgraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novaIgraToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.izadjiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kakoSeIgraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.SystemColors.Menu;
+            this.a1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.a1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.a1.FlatAppearance.BorderSize = 0;
+            this.a1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.a1.Font = new System.Drawing.Font("Ravie", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.Location = new System.Drawing.Point(36, 38);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(100, 100);
+            this.a1.TabIndex = 0;
+            this.a1.UseVisualStyleBackColor = false;
+            this.a1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.SystemColors.Menu;
+            this.a2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.a2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.a2.FlatAppearance.BorderSize = 0;
+            this.a2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.a2.Font = new System.Drawing.Font("Ravie", 27.75F, System.Drawing.FontStyle.Bold);
+            this.a2.Location = new System.Drawing.Point(142, 38);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(100, 100);
+            this.a2.TabIndex = 1;
+            this.a2.UseVisualStyleBackColor = false;
+            this.a2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // a3
+            // 
+            this.a3.BackColor = System.Drawing.SystemColors.Menu;
+            this.a3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.a3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.a3.FlatAppearance.BorderSize = 0;
+            this.a3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.a3.Font = new System.Drawing.Font("Ravie", 27.75F, System.Drawing.FontStyle.Bold);
+            this.a3.Location = new System.Drawing.Point(248, 38);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(100, 100);
+            this.a3.TabIndex = 2;
+            this.a3.UseVisualStyleBackColor = false;
+            this.a3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.SystemColors.Menu;
+            this.b1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.b1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.b1.FlatAppearance.BorderSize = 0;
+            this.b1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b1.Font = new System.Drawing.Font("Ravie", 27.75F, System.Drawing.FontStyle.Bold);
+            this.b1.Location = new System.Drawing.Point(36, 144);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(100, 100);
+            this.b1.TabIndex = 3;
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.SystemColors.Menu;
+            this.b2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.b2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.b2.FlatAppearance.BorderSize = 0;
+            this.b2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b2.Font = new System.Drawing.Font("Ravie", 27.75F, System.Drawing.FontStyle.Bold);
+            this.b2.Location = new System.Drawing.Point(142, 144);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(100, 100);
+            this.b2.TabIndex = 4;
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.SystemColors.Menu;
+            this.b3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.b3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.b3.FlatAppearance.BorderSize = 0;
+            this.b3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b3.Font = new System.Drawing.Font("Ravie", 27.75F, System.Drawing.FontStyle.Bold);
+            this.b3.Location = new System.Drawing.Point(248, 144);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(100, 100);
+            this.b3.TabIndex = 5;
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // c1
+            // 
+            this.c1.BackColor = System.Drawing.SystemColors.Menu;
+            this.c1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.c1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.c1.FlatAppearance.BorderSize = 0;
+            this.c1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.c1.Font = new System.Drawing.Font("Ravie", 27.75F, System.Drawing.FontStyle.Bold);
+            this.c1.Location = new System.Drawing.Point(36, 250);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(100, 100);
+            this.c1.TabIndex = 6;
+            this.c1.UseVisualStyleBackColor = false;
+            this.c1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // c2
+            // 
+            this.c2.BackColor = System.Drawing.SystemColors.Menu;
+            this.c2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.c2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.c2.FlatAppearance.BorderSize = 0;
+            this.c2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.c2.Font = new System.Drawing.Font("Ravie", 27.75F, System.Drawing.FontStyle.Bold);
+            this.c2.Location = new System.Drawing.Point(142, 250);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(100, 100);
+            this.c2.TabIndex = 7;
+            this.c2.UseVisualStyleBackColor = false;
+            this.c2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // c3
+            // 
+            this.c3.BackColor = System.Drawing.SystemColors.Menu;
+            this.c3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.c3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.c3.FlatAppearance.BorderSize = 0;
+            this.c3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.c3.Font = new System.Drawing.Font("Ravie", 27.75F, System.Drawing.FontStyle.Bold);
+            this.c3.Location = new System.Drawing.Point(248, 250);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(100, 100);
+            this.c3.TabIndex = 8;
+            this.c3.UseVisualStyleBackColor = false;
+            this.c3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // IGRA
+            // 
+            this.IGRA.Name = "IGRA";
+            this.IGRA.Size = new System.Drawing.Size(61, 4);
+            this.IGRA.Text = "Igra";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novaIgraToolStripMenuItem,
+            this.infoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(384, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // novaIgraToolStripMenuItem
+            // 
+            this.novaIgraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novaIgraToolStripMenuItem1,
+            this.izadjiToolStripMenuItem});
+            this.novaIgraToolStripMenuItem.Name = "novaIgraToolStripMenuItem";
+            this.novaIgraToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.novaIgraToolStripMenuItem.Text = "Igra";
+            // 
+            // novaIgraToolStripMenuItem1
+            // 
+            this.novaIgraToolStripMenuItem1.Name = "novaIgraToolStripMenuItem1";
+            this.novaIgraToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.novaIgraToolStripMenuItem1.Text = "Nova igra";
+            this.novaIgraToolStripMenuItem1.Click += new System.EventHandler(this.novaIgraToolStripMenuItem1_Click);
+            // 
+            // izadjiToolStripMenuItem
+            // 
+            this.izadjiToolStripMenuItem.Name = "izadjiToolStripMenuItem";
+            this.izadjiToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.izadjiToolStripMenuItem.Text = "Izadji";
+            this.izadjiToolStripMenuItem.Click += new System.EventHandler(this.izadjiToolStripMenuItem_Click);
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.kakoSeIgraToolStripMenuItem});
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.infoToolStripMenuItem.Text = "Info";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.helpToolStripMenuItem.Text = "infoooo";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // kakoSeIgraToolStripMenuItem
+            // 
+            this.kakoSeIgraToolStripMenuItem.Name = "kakoSeIgraToolStripMenuItem";
+            this.kakoSeIgraToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kakoSeIgraToolStripMenuItem.Text = "Kako se igra?";
+            this.kakoSeIgraToolStripMenuItem.Click += new System.EventHandler(this.kakoSeIgraToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 12F);
+            this.label1.Location = new System.Drawing.Point(32, 366);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 22);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "IGRAC X JE NA POTEZU";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(384, 411);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Iks-Oks";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.ContextMenuStrip IGRA;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem novaIgraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novaIgraToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem izadjiToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kakoSeIgraToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iksoks
+{
+    public partial class Form1 : Form
+    {
+        bool potez = false;
+        int count = 0;
+        public void Provera()
+        {
+            if ((a1.Text == a2.Text && a2.Text == a3.Text && a1.Enabled == false)|| (b1.Text==b2.Text && b2.Text==b3.Text&& b1.Enabled == false) || (c1.Text == c2.Text && c2.Text == c3.Text&& c1.Enabled == false))
+                Pobeda();
+            if ((a1.Text==b1.Text && b1.Text==c1.Text && a1.Enabled==false) || (a2.Text == b2.Text && b2.Text == c2.Text && a2.Enabled == false) || (a3.Text == b3.Text && b3.Text == c3.Text && a3.Enabled == false))
+            Pobeda(); 
+            if ((a1.Text == b2.Text && b2.Text == c3.Text && a1.Enabled == false) || (a3.Text == b2.Text && b2.Text == c1.Text && a3.Enabled == false))
+            Pobeda();
+            if (count == 9)
+            {
+                MessageBox.Show("NERESENO");
+                Reset();
+                
+            }
+        }
+        public void Reset()
+        {
+            a1.Enabled = a2.Enabled = a3.Enabled = b1.Enabled = b2.Enabled = b3.Enabled = c1.Enabled = c2.Enabled = c3.Enabled = true;
+            a1.Text = a2.Text = a3.Text = b1.Text = b2.Text = b3.Text = c1.Text = c2.Text = c3.Text = "";
+            count = 0;
+        }
+        public void Pobeda()
+        {
+            MessageBox.Show(potez ? "Pobednik je X" : "Pobednik je O");
+            potez = !potez;
+            Reset();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void novaIgraToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void izadjiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button dugme = (Button)sender;
+            if (dugme.Enabled == true)
+            {
+                dugme.Enabled = false;
+                dugme.Text = potez ? "O" : "X";
+                
+                potez = !potez;
+                count++;               
+                Provera();
+                label1.Text = potez ? "IGRAC O JE NA POTEZU" : "IGRAC X JE NA POTEZU";
+
+            }
+
+
+
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("IKS OKS\nNapravljeno iz dosade\nNovembar 2022.\nDimitrije Sovljanski");
+        }
+
+        private void kakoSeIgraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                MessageBox.Show("?XD");
+            }
+        }
+    }
+}
